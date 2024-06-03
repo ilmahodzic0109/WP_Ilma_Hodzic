8ca26e9cc987daa8dd3efcd9e92fd362a7b40b33
adnan <ilmahodzic7@gmail.com>
2024-06-04 01:53:00 +0200

    deployment

--- backend/public/v1/docs/swagger.php
+++ backend/public/v1/docs/swagger.php
@@ -2,11 +2,16 @@
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
-define('BASE_URL', 'http://localhost:80/WP_Ilma_Hodzic/backend/');
-
+//define('BASE_URL', 'http://localhost:80/WPEmina/furni-backend/');
+if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
+    define('BASE_URL', 'http://localhost:80/WP_Ilma_Hodzic/backend/');
+} else {
+    define('BASE_URL', 'https://shark-app-qoir3.ondigitalocean.app/backend/');
+}
 error_reporting(0);
 
 $openapi = \OpenApi\Generator::scan(['../../../rest/routes', './']);
 header('Content-Type: application/x-yaml');
 echo $openapi->toYaml();
 ?>
+
--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,3 +1,15 @@
 var Constants = {
-    API_BASE_URL: "http://localhost:80/WP_Ilma_Hodzic/backend/",
+  get_api_base_url: function () {
+    if(location.hostname == 'localhost'){
+      return "http://localhost:80/WP_Ilma_Hodzic/backend/";
+    } else {
+      return "https://shark-app-qoir3.ondigitalocean.app/backend/";
+    }
+  }
+    //API_BASE_URL: "http://localhost:80/WPEmina/furni-backend/",
   };
+
+
+/*var Constants = {
+    API_BASE_URL: "http://localhost:80/WP_Ilma_Hodzic/backend/",
+  };*/
